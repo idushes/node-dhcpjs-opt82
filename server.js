bdec9280bb5bc175726bd05b98af0a70347d23b1
root <root@node.home-nadym.ru>
2016-03-28 05:00:21 +0500

    parse ipv6 rfc3646

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var util = require('util');
 var dhcpjs = require('./lib');
 var console = require('better-console');
 var billing = require('./lib/billing');
+var packet6 = require('./lib/packet6');
 var config = require('./config.json');
 
 
@@ -93,12 +94,54 @@ server.on('dhcpInform', function(m,rinfo) {
 server.on('dhcpV6RelayForward', function(m,rinfo) {
     console.log(getDate() + '  ' + rinfo.address + ' ->  dhcpV6RelayForward');
     console.log(m);
+    console.log(rinfo);
     console.log(m.options.dhcpRelayMessage);
+    if (m.options.dhcpRelayMessage) {
+        var message = m.options.dhcpRelayMessage;
+        console.log('*');
+        console.log(message);
+        var pkt = {
+            xid: message.xid,
+            dhcpMessageType: dhcpjs.Protocol.DHCPv6MessageType.ADVERTISE,
+            options: {
+                clientIdentifierOption: message.options.clientIdentifierOption,
+                serverIdentifierOption: { linkLayerAddress: "de:b2:0d:14:03:2c" }
+            }
+        };
+        var buf = packet6.createPacket(pkt);
+        var bufRelay = packet6.createPacketRelayReply(m,buf);
+        console.warn(bufRelay);
+
+
+        server.sendPacket6(bufRelay, {host: rinfo.address, port: rinfo.port}, function () {
+            console.info("RELAY_REPL (ADVERTISE) sent");
+        });
+        server.sendPacket6(buf, {host: rinfo.address, port: rinfo.port}, function () {
+            console.info("ADVERTISE sent");
+        });
+    }
+
+
 });
 
 server.on('dhcpV6Renew', function(m,rinfo) {
     console.log(getDate() + '  ' + rinfo.address +' ->  dhcpV6Renew');
     console.log(m);
+    var pkt = {
+        xid: m.xid,
+        dhcpMessageType: dhcpjs.Protocol.DHCPv6MessageType.ADVERTISE,
+        options: {
+            clientIdentifierOption: m.options.clientIdentifierOption,
+            serverIdentifierOption: { linkLayerAddress: "de:b2:0d:14:03:2c" }
+        }
+
+    };
+    var buf = packet6.createPacket(pkt);
+    console.warn(buf);
+    // server.sendPacket6(buf, {host: rinfo.address, port: rinfo.port}, function () {
+    //     console.info("ADVERTISE sent");
+    // });
+
 });
 
 
